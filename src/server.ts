e7bdb7effaf038917f720833b091baade0c2ae7e
eltoncampos1 <eltoncampos36@gmail.com>
2021-10-15 01:13:22 -0300

    feat: create doc to createCategory using swagger api

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import { router } from './routes';
+import swagger from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(3333, () => console.log('Server running'));
